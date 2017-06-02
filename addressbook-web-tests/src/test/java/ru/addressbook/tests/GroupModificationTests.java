c6f239024d3bacaf94f1abb042f501a2507dbc24
rqip <rqip@qip.ru>
2017-06-02 17:33:29 +0300

    for 10-2 +

--- addressbook-web-tests/src/test/java/ru/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/tests/GroupModificationTests.java
@@ -1,5 +1,5 @@
 package ru.addressbook.tests;
-
+// reload for 10-2
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -13,27 +13,23 @@ public class GroupModificationTests extends TestBase {
 
   @BeforeMethod
   public void ensurePreconditions() {
-    if (app.db().groups().size() == 0) {
-      app.goTo().groupPage();
+    app.goTo().groupPage();
+    if (app.group().all().size() == 0) {
       app.group().create(new GroupData().withName("test1"));
     }
-
   }
 
   @Test
   public void testGroupModification() {
-    Groups before = app.db().groups();
+    Groups before = app.group().all();
     GroupData modifiedGroup = before.iterator().next();
     GroupData group = new GroupData().withId(modifiedGroup.getId()).
             withName("test1").withHeader("test2").withFooter("test3");
 
-    app.goTo().groupPage();
     app.group().modify(group);
     assertThat(app.group().count(), equalTo(before.size()));
-    Groups after = app.db().groups();
+    Groups after = app.group().all();
     assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
-
-    verifyGroupListInUi();
   }
 
 }
