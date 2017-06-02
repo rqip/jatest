2886b555d00ae8044331fc09172cfc29ccd3823a
rqip <rqip@qip.ru>
2017-06-02 17:28:11 +0300

    for 10-2 + TestBase

--- addressbook-web-tests/src/test/java/ru/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/tests/TestBase.java
@@ -1,61 +1,22 @@
 package ru.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.addressbook.appmanager.AppManager;
-import ru.addressbook.model.GroupData;
-import ru.addressbook.model.Groups;
-
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestBase {
 
-  Logger logger = LoggerFactory.getLogger(TestBase.class);
-
-  protected static final AppManager app
-          = new AppManager(System.getProperty("browser", BrowserType.FIREFOX));
+  protected static final AppManager app = new AppManager(BrowserType.FIREFOX);
 
   @BeforeSuite
   public void setUp() throws Exception {
     app.init();
    }
 
-  @AfterSuite(alwaysRun = true)
+  @AfterSuite
   public void tearDown() {
       app.stop();
-  }
-
-  @BeforeMethod
-  public void logTestStart(Method m, Object[] p) {
-    logger.info("Start test " + m.getName() + " with params "
-            + Arrays.asList(p));
-  }
-
-  @AfterMethod(alwaysRun = true)
-  public void logTestStop(Method m) {
-    logger.info("Stop test " + m.getName());
-  }
-
-  public void verifyGroupListInUi() {
-    if (Boolean.getBoolean("verifyUI")) {
-
-      Groups dbGroups = app.db().groups();
-      Groups uiGroups = app.group().all();
-      assertThat(uiGroups, equalTo(dbGroups.stream().map((g) -> new GroupData()
-              .withId(g.getId()).withName(g.getName()))
-              .collect(Collectors.toSet()));
-    }
   }
 
 }
