5af215fdc367b34bcaca85e4a251840d805776ff
rqip <rqip@qip.ru>
2017-06-02 17:01:47 +0300

    for 10-2

--- addressbook-web-tests/src/test/java/ru/addressbook/appmanager/AppManager.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/appmanager/AppManager.java
@@ -1,15 +1,25 @@
 package ru.addressbook.appmanager;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+import java.util.Properties;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 import java.util.concurrent.TimeUnit;
 
 public class AppManager {
 
+  private final Properties properties;
   WebDriver wd;
 
   private SessionHelper sessionHelper;
@@ -20,15 +30,28 @@ public class AppManager {
 
   public AppManager(String browser) {
     this.browser = browser;
+    properties = new Properties();
   }
 
-  public void init() {
-    if (browser.equals(BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.IE)) {
-      wd = new InternetExplorerDriver();
-    } else if (browser.equals(BrowserType.CHROME)) {
-      wd = new ChromeDriver();
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(
+            new File(String.format("src/test/resources/%s.properties", target))));
+
+    // DbHelper dbHelper = new DbHelper();
+
+    if ("".equals(properties.getProperty("selenium.server"))) {
+      if (browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.IE)) {
+        wd = new InternetExplorerDriver();
+      } else if (browser.equals(BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+      }
+    } else {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setBrowserName(browser);
+      wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
 
     wd.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);  //60
